86aa6cd727973ad916ae5afaa24484edf4744649
michellefishe <michellefisher@verizon.net>
2023-01-20 22:00:22 -0700

    folder rename

--- pantry-app/src/App.js
+++ pantry-app/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const appName = "PANTRY";
+  const appLogo = document.createElement("img");
+  appLogo.src = "pantry-app/generalPlanning/UIDev/logo.png";
   return (
     <div className="App">
       <header className="App-header">
